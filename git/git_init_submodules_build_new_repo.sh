e163312d2c4827d61305b2f52d061b5332e5d78a
mivalt.filip <mivalt.filip@gmail.com>
2024-04-25 12:41:09 -0500

    Test commit

--- git/git_init_submodules_build_new_repo.sh
+++ git/git_init_submodules_build_new_repo.sh
@@ -3,9 +3,13 @@ REPOSITORIES=("test_subtree1" "test_subtree2")
 
 for REPO in "${REPOSITORIES[@]}"
 do
-  #echo https://github.com/$GITUSER/$REPO.git
+  SUBDIR=$(echo "$REPO" | cut -d'_' -f2)
+
+  echo $SUBDIR
+
   git remote add $REPO https://github.com/$GITUSER/$REPO.git
-  git subtree add --prefix=brainmaze/$REPO $REPO dev --squash
   git remote set-url subtree1 git@github.com:$GITUSER/$REPO.git
+  git subtree add --prefix=brainmaze/$SUBDIR $REPO dev/brainmaze --squash # run this line only if you need a brand new. This will copy the folders from other repos.
 done
+
 
